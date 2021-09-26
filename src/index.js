8bafe9096e8a091792de9f92ef1ce9dd67f0cba2
Lucky <vagyasri2011@gmail.com>
2021-09-26 19:06:02 +0200

    Update html file Create An array in JS Give 3 key value pair to index.js Add some style in the CSS file

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+import './style.css';
+
+const container = document.querySelector('.list-container');
+
+const todoList = [{
+  index: 1,
+  description: 'Wash the Dishes',
+  completed: true,
+},
+{
+  index: 2,
+  description: 'Complete to-do list Project',
+  completed: true,
+}];
+
