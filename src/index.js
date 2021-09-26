02ae391d760885d8dabab582d98f57827e1b01cc
Lucky <vagyasri2011@gmail.com>
2021-09-26 19:10:33 +0200

    Add a function to js file to render the elements Add styling accordingly

--- src/index.js
+++ src/index.js
@@ -14,3 +14,30 @@ const todoList = [{
   completed: true,
 }];
 
+function clear(element) {
+  while (element.firstChild) element.removeChild(element.firstChild);
+}
+
+function render() {
+  clear(container);
+  todoList.forEach((todo) => {
+    const div = document.createElement('div');
+    const label = document.createElement('label');
+    const input = document.createElement('input');
+    input.type = 'checkbox';
+    input.name = 'name';
+    input.value = 'value';
+    input.id = 'id';
+    input.classList.add('strikethrough');
+    const listElement = document.createElement('li');
+    div.classList.add('listcont-prop');
+    listElement.classList.add('list-prop');
+    listElement.innerHTML = `${todo.description}<i class="fas fa-ellipsis-v"></i>`;
+    div.appendChild(label);
+    div.appendChild(listElement);
+    label.appendChild(input);
+    container.appendChild(div);
+  });
+}
+
+render();
