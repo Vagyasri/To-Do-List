85fdc5d23e0ecbec1185de1b779d049bb5e480c3
Lucky <vagyasri2011@gmail.com>
2021-09-27 09:24:48 +0200

    Import function.js as a module into app.js

--- src/function.js
+++ src/function.js
@@ -1,13 +1,13 @@
 const LIST_KEY = 'task.list';
 const SELECT_LIST = 'select.list';
 const todoList = JSON.parse(localStorage.getItem(LIST_KEY)) || [];
-const selectList = localStorage.getItem(SELECT_LIST);
+export const selectList = localStorage.getItem(SELECT_LIST);
 
-const taskComp = (todo, input) => {
+export const taskComp = (todo, input) => {
   todo.completed = input.checked;
 };
 
-const save = () => {
+export const save = () => {
   localStorage.setItem(LIST_KEY, JSON.stringify(todoList));
   localStorage.setItem(SELECT_LIST, selectList);
 };
