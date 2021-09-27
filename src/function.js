5fcc18eaffeb29b029727499c2b2ff288344fa63
Lucky <vagyasri2011@gmail.com>
2021-09-27 15:38:02 +0200

    Update function.js

--- src/function.js
+++ src/function.js
@@ -1,7 +1,5 @@
 const LIST_KEY = 'task.list';
-const SELECT_LIST = 'select.list';
 export const todoList = JSON.parse(localStorage.getItem(LIST_KEY)) || [];
-export const selectList = localStorage.getItem(SELECT_LIST);
 
 export const taskComp = (todo, input) => {
   todo.completed = input.checked;
@@ -9,5 +7,4 @@ export const taskComp = (todo, input) => {
 
 export const save = () => {
   localStorage.setItem(LIST_KEY, JSON.stringify(todoList));
-  localStorage.setItem(SELECT_LIST, selectList);
 };
