8efc4e0c5c5d2ae2ec32f47ddbb071f0e312c5d5
Lucky <vagyasri2011@gmail.com>
2021-09-27 09:23:14 +0200

    Save into local Storage

--- src/function.js
+++ src/function.js
@@ -1,5 +1,13 @@
 const LIST_KEY = 'task.list';
 const SELECT_LIST = 'select.list';
 const todoList = JSON.parse(localStorage.getItem(LIST_KEY)) || [];
-export const selectList = localStorage.getItem(SELECT_LIST);
+const selectList = localStorage.getItem(SELECT_LIST);
 
+const taskComp = (todo, input) => {
+  todo.completed = input.checked;
+};
+
+const save = () => {
+  localStorage.setItem(LIST_KEY, JSON.stringify(todoList));
+  localStorage.setItem(SELECT_LIST, selectList);
+};
